f02a6cfd0d62951c5bc4414c8aa5240985103a31
Craig <craigdurkin@gmail.com>
2015-07-13 13:31:14 -0400

    comment out devise stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token, if: :json_request?
+  #protect_from_forgery with: :exception
+  #skip_before_action :verify_authenticity_token, if: :json_request?
   #before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :configure_permitted_parameters, if: :devise_controller?
+  http_basic_authenticate_with name: "dougs", password: "dougs"
 
   protected
 
